c9a63c9fbc86cecd28dc2620ee6f13594e9e712b
Ubuntu <ubuntu@ip-172-31-69-61.ec2.internal>
2020-10-10 20:29:47 +0000

    bertflowdelta + handler script

---
+++ BertFlowDelta/predictions_formatter.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+raw = sys.argv[1]
+res = open("formatted_predictions.json", "w")
+with open(raw) as f:
+    dic = eval(f.read())
+    ids = set()
+    for key in dic:
+        id_ = key.rsplit("#", 1)[0]
+        if id_ not in ids:
+            ids.add(id_)
+    # print(len(ids))
+    for id_ in ids:
+        best_span_str = []
+        qids = []
+        num = 0
+        qid = id_ + "#" + str(num)
+        while qid in dic:
+            qids.append(qid)
+            best_span_str.append(dic[qid])
+            num += 1
+            qid = id_ + "#" + str(num)
+        res.write(json.dumps({"best_span_str": best_span_str, "qid": qids, "yesno": ["y"] * len(qids), "followup": ["y"] * len(qids)}))
+        res.write('\n')
+res.close()
