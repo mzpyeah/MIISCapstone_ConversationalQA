493bc77a566490e738256cf30db50cfc51735c07
Ubuntu <ubuntu@ip-172-31-66-14.ec2.internal>
2020-10-15 17:31:19 +0000

    rm redundant conda envs

---
+++ rm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+conda env remove -y --name aws_neuron_mxnet_p36
+conda env remove -y --name aws_neuron_pytorch_p36
+conda env remove -y --name aws_neuron_tensorflow_p36
+conda env remove -y --name chainer_p27
+conda env remove -y --name chainer_p36
+conda env remove -y --name mxnet_latest_p37
+conda env remove -y --name mxnet_p36
+conda env remove -y --name mxnet_p27
